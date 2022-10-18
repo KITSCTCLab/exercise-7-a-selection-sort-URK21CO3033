ae3fadb1ca5089ee0a739c0a6bcdce26d72e9044
URK21CO3033 <110015522+URK21CO3033@users.noreply.github.com>
2022-10-18 12:02:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,53 +1,21 @@
-class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+from typing import List
 
-
-class Queue:
-  def __init__(self):
-    self.head = None
-    self.last = None
-
-  def enqueue(self, data) -> None:
-    # Write your code here
-    if self.last is None:
-      self.head = Node(data)
-      self.last = self.head
-    else:
-      self.last.next = Node(data)
-      self.last = self.last.next
-
-  def dequeue(self) -> None:
-    # Write your code here
-    if self.head is None:
-      return None
-    else:
-      val_returned = self.head.data
-      self.head = self.head.next
-
-  def status(self) -> None:
-    # Write your code here
-    current = self.head
-    status_of_queue = []
-    while (current):
-      status_of_queue.append(current.data)
-      current = current.next
-    for element in status_of_queue:
-      print(element, end="=>")
-    print(None)
-
+def selectionSort(array, size) -> List[int]:
+  # Write your code here
+  for i in range(len(array)):
+    minimum = i
+    for j in range(i+1,len(array)):
+      if array[minimum]>array[j]:
+        minimum=j
+    array[i],array[minimum] = array[minimum],array[i]
+  return array
 
 # Do not change the following code
-queue = Queue()
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
 input_data = input()
-data = input_data.split(',')
-for i in range(len(operations)):
-  if operations[i] == "enqueue":
-    queue.enqueue(int(data[i]))
-  elif operations[i] == "dequeue":
-    queue.dequeue()
-queue.status()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(selectionSort(data, len(data)))
